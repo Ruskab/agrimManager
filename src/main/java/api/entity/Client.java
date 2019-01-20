4e003d1d4234246f2b75a8a806329c107368a590
ilya <ilya.kab@gmail.com>
2019-01-20 23:36:07 +0100

    Entidad Client con anotaciones de JPA #4

---
+++ src/main/java/api/entity/Client.java
@@ -0,0 +1,57 @@
+package api.entity;
+
+import javax.persistence.*;
+
+@Entity
+
+@Table(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = true, length = 200)
+    private String fullName;
+
+    @Column(nullable = true)
+    private int hours;
+
+    public Client(){
+        //JPA
+    }
+
+    public Client(String fullName, int hours){
+        this.fullName = fullName;
+        this.hours = hours;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", hours=" + hours +
+                '}';
+    }
+}
