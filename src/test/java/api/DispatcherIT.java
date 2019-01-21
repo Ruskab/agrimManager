8de19da8d0809b9a163802faa1e2c62a3cd74fdf
ilya <ilya.kab@gmail.com>
2019-01-21 18:54:03 +0100

    primera version del test de integracion del dispatcher, con diferentes casos de test para el post clients #8

---
+++ src/test/java/api/DispatcherIT.java
@@ -0,0 +1,50 @@
+package api;
+
+import api.apiControllers.ClientApiController;
+import api.dtos.ClientDto;
+import http.Client;
+import http.HttpException;
+import http.HttpRequest;
+import http.HttpStatus;
+import org.junit.jupiter.api.*;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+public class DispatcherIT {
+
+    @Test
+    void testCreateClient() {
+        HttpRequest request = HttpRequest.builder(ClientApiController.CLIENTS)
+                .body(new ClientDto("fullNameTest",1)).post();
+
+        int id = (int) new Client().submit(request).getBody();
+
+        assertThat(id, is(not(0)));
+    }
+
+    @Test
+    void testClientInvalidRequest() {
+        HttpRequest request = HttpRequest.builder(ClientApiController.CLIENTS).path("/invalid").body(null).post();
+
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertThat(HttpStatus.BAD_REQUEST, is(exception.getHttpStatus()));
+    }
+
+    @Test
+    void testCreateUserWithoutClientDto() {
+        HttpRequest request = HttpRequest.builder(ClientApiController.CLIENTS).body(null).post();
+
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertThat(HttpStatus.BAD_REQUEST, is(exception.getHttpStatus()));
+    }
+
+    @Test
+    void testCreateUserWithoutClientDtoFullName() {
+        HttpRequest request = HttpRequest.builder(ClientApiController.CLIENTS).body(new ClientDto(null,1)).post();
+
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertThat(HttpStatus.BAD_REQUEST, is(exception.getHttpStatus()));
+    }
+}
