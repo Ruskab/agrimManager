a8e9b162cffed5b7271be56f66cec22c77793724
ilya <ilya.kab@gmail.com>
2019-01-27 21:24:52 +0100

    Dispatcher::doPut cambiada la excepcion a NotFoundException en caso que de el path no coincida. ClientApiController::update añadido validador de id para verificar que sea numerico y añadido test de integracion para el metodo update issue #12

--- src/test/java/api/DispatcherIT.java
+++ src/test/java/api/DispatcherIT.java
@@ -7,13 +7,11 @@ import http.HttpException;
 import http.HttpRequest;
 import http.HttpStatus;
 import org.junit.jupiter.api.*;
-import org.mockito.Mock;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
 
 public class DispatcherIT {
 
@@ -49,5 +47,32 @@ public class DispatcherIT {
 
         HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
         assertThat(HttpStatus.BAD_REQUEST, is(exception.getHttpStatus()));
+    }
+
+    @Test
+    void testUpdateClientWithoutClientDtoFullName() {
+        String id = "1";
+        HttpRequest request = HttpRequest.builder(ClientApiController.CLIENTS).path(ClientApiController.ID_ID)
+                .expandPath(id).body(new ClientDto(null,1)).put();
+
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertThat(exception.getHttpStatus(), is(HttpStatus.BAD_REQUEST));
+    }
+
+    @Test
+    void testUpdateUserWithoutUserDto() {
+        String id = "1";
+        HttpRequest request = HttpRequest.builder(ClientApiController.CLIENTS).path(ClientApiController.ID_ID)
+                .expandPath(id).body(null).put();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.BAD_REQUEST, exception.getHttpStatus());
+    }
+
+    @Test
+    void testUpdateUserNotFoundException() {
+        HttpRequest request = HttpRequest.builder(ClientApiController.CLIENTS).path(ClientApiController.ID_ID)
+                .expandPath("s5FdeGf54D").body(new ClientDto("updatedName",1)).put();
+        HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
+        assertEquals(HttpStatus.NOT_FOUND, exception.getHttpStatus());
     }
 }
