8794f982435e5568a5c420bcf0623368edd2a3a8
ilya <ilya.kab@gmail.com>
2019-01-28 20:13:41 +0100

    DispatcherIT update client IT, mejorado create Client IT #12

--- src/test/java/api/DispatcherIT.java
+++ src/test/java/api/DispatcherIT.java
@@ -1,6 +1,9 @@
 package api;
 
 import api.apiControllers.ClientApiController;
+import api.businessControllers.ClientBusinessController;
+import api.daos.DaoFactory;
+import api.daos.hibernate.DaoFactoryHibr;
 import api.dtos.ClientDto;
 import http.Client;
 import http.HttpException;
@@ -8,21 +11,39 @@ import http.HttpRequest;
 import http.HttpStatus;
 import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DispatcherIT {
 
+    private static ClientBusinessController clientBusinessController;
+    private static List<Integer> createdUsers;
+
+    @BeforeAll
+    static void prepare() {
+        createdUsers = new ArrayList<>();
+        DaoFactory.setFactory(new DaoFactoryHibr());
+        clientBusinessController = new ClientBusinessController();
+        createdUsers.add(clientBusinessController.create(new ClientDto("fakeFullNameTest", 1)));
+        createdUsers.add(clientBusinessController.create(new ClientDto("fakeFullNameTest2", 2)));
+    }
+
     @Test
     void testCreateClient() {
         HttpRequest request = HttpRequest.builder(ClientApiController.CLIENTS)
-                .body(new ClientDto("fullNameTest", 1)).post();
+                .body(new ClientDto("fullNameTest", 4)).post();
 
         int id = (int) new Client().submit(request).getBody();
+        createdUsers.add(id);
 
-        assertThat(id, is(not(0)));
+        Optional<api.entity.Client> updatedUser = DaoFactory.getFactory().getClientDao().read(id);
+        assertThat(updatedUser.get().getFullName(), is("fullNameTest"));
+        assertThat(updatedUser.get().getHours(), is(4));
     }
 
     @Test
@@ -47,6 +68,21 @@ public class DispatcherIT {
 
         HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
         assertThat(HttpStatus.BAD_REQUEST, is(exception.getHttpStatus()));
+    }
+
+    @Test
+    public void testUpdateClient(){
+        int createdUserId = createdUsers.get(0);
+        String createdUserFullName = DaoFactory.getFactory().getClientDao().read(createdUserId).get().getFullName();
+
+        HttpRequest request = HttpRequest.builder(ClientApiController.CLIENTS).path(ClientApiController.ID_ID)
+                .expandPath(Integer.toString(createdUserId)).body(new ClientDto("updatedName", 3)).put();
+        new Client().submit(request);
+        Optional<api.entity.Client> updatedUser = DaoFactory.getFactory().getClientDao().read(createdUserId);
+
+        assertThat(createdUserFullName, is("fakeFullNameTest"  ));
+        assertThat(updatedUser.get().getFullName(), is("updatedName"));
+        assertThat(updatedUser.get().getHours(), is(3));
     }
 
     @Test
@@ -75,4 +111,10 @@ public class DispatcherIT {
         HttpException exception = assertThrows(HttpException.class, () -> new Client().submit(request));
         assertEquals(HttpStatus.NOT_FOUND, exception.getHttpStatus());
     }
+
+    @AfterAll
+    static void deleteCreatedUsers(){
+        createdUsers.forEach(id -> DaoFactory.getFactory().getClientDao().deleteById(id));
+    }
+
 }
