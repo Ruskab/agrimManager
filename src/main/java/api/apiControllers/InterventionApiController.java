a01d0f4f6d387e4c0d991380b65b06bf0fb5e281
ilya <ilya.kab@gmail.com>
2019-02-23 14:17:04 +0100

    create intervention Dispatcher + apiController issue #20

---
+++ src/main/java/api/apiControllers/InterventionApiController.java
@@ -0,0 +1,35 @@
+package api.apiControllers;
+
+import api.businessControllers.InterventionBusinesssController;
+import api.dtos.InterventionDto;
+import api.entity.State;
+import api.exceptions.ArgumentNotValidException;
+import api.exceptions.NotFoundException;
+import com.mysql.cj.core.util.StringUtils;
+
+public class InterventionApiController {
+    public static final String INTERVENTIONS = "/interventions";
+
+    public static final String ID = "/{id}";
+
+    InterventionBusinesssController interventionBusinessController = new InterventionBusinesssController();
+
+    public int create(InterventionDto interventionDto) {
+        this.validate(interventionDto, "interventionDto");
+        this.validate(interventionDto.getState(), "State");
+        return interventionBusinessController.create(interventionDto);
+    }
+
+    private void validate(Object property, String message) {
+        if (property == null) {
+            throw new ArgumentNotValidException(message + " is missing");
+        }
+    }
+
+    private void validateId(String id, String message) {
+        if (!StringUtils.isStrictlyNumeric(id)) {
+            throw new NotFoundException(message + " Should be numeric");
+            //todo change to BadRequestException
+        }
+    }
+}
