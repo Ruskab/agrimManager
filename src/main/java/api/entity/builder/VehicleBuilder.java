956096caa7089fc79b6a48e5c79b01ff961952d2
ilya <ilya.kab@gmail.com>
2019-03-01 20:00:22 +0100

    refactor: mejorar el builder de vehiculo, quitar codigo duplicado, constrctor demasiado largo y vehicle by default

--- src/main/java/api/entity/builder/VehicleBuilder.java
+++ src/main/java/api/entity/builder/VehicleBuilder.java
@@ -6,80 +6,92 @@ import api.entity.Vehicle;
 import java.time.LocalDate;
 
 public class VehicleBuilder {
-    private String registrationPlate;
-    private String brand;
-    private Client client;
-    private String kms;
-    private String bodyOnFrame;
-    private LocalDate lastRevisionDate;
-    private LocalDate itvDate;
-    private LocalDate nextItvDate;
-    private String airFilterReference;
-    private String oilFilterReference;
-    private String fuelFilter;
-    private String motorOil;
+
+    private Vehicle vehicle;
+
+    public VehicleBuilder(String registrationPlate) {
+        this.vehicle = new Vehicle(registrationPlate);
+    }
 
     public VehicleBuilder setRegistrationPlate(String registrationPlate) {
-        this.registrationPlate = registrationPlate;
+        this.vehicle.setRegistrationPlate(registrationPlate);
         return this;
     }
 
     public VehicleBuilder setBrand(String brand) {
-        this.brand = brand;
+        this.vehicle.setBrand(brand);
         return this;
     }
 
     public VehicleBuilder setClient(Client client) {
-        this.client = client;
+        this.vehicle.setClient(client);
         return this;
     }
 
     public VehicleBuilder setKMS(String kms) {
-        this.kms = kms;
+        this.vehicle.setKms(kms);
         return this;
     }
 
     public VehicleBuilder setBodyOnFrame(String bodyOnFrame) {
-        this.bodyOnFrame = bodyOnFrame;
+        this.vehicle.setBodyOnFrame(bodyOnFrame);
         return this;
     }
 
     public VehicleBuilder setLastRevisionDate(LocalDate lastRevisionDate) {
-        this.lastRevisionDate = lastRevisionDate;
+        this.vehicle.setLastRevisionDate(lastRevisionDate);
         return this;
     }
 
     public VehicleBuilder setItvDate(LocalDate itvDate) {
-        this.itvDate = itvDate;
+        this.vehicle.setItvDate(itvDate);
         return this;
     }
 
     public VehicleBuilder setNextItvDate(LocalDate nextItvDate) {
-        this.nextItvDate = nextItvDate;
+        this.vehicle.setNextItvDate(nextItvDate);
         return this;
     }
 
     public VehicleBuilder setAirFilterReference(String airFilterReference) {
-        this.airFilterReference = airFilterReference;
+        this.vehicle.setAirFilterReference(airFilterReference);
         return this;
     }
 
     public VehicleBuilder setOilFilterReference(String oilFilterReference) {
-        this.oilFilterReference = oilFilterReference;
+        this.vehicle.setOilFilterReference(oilFilterReference);
         return this;
     }
 
     public VehicleBuilder setFuelFilter(String fuelFilter) {
-        this.fuelFilter = fuelFilter;
+        this.vehicle.setFuelFilter(fuelFilter);
         return this;
     }
 
     public VehicleBuilder setMotorOil(String motorOil) {
-        this.motorOil = motorOil;
+        this.vehicle.setMotorOil(motorOil);
         return this;
     }
 
-    public Vehicle createVehicle() {
-        return new Vehicle(registrationPlate, brand, client, kms, bodyOnFrame, lastRevisionDate, itvDate, nextItvDate, airFilterReference, oilFilterReference, fuelFilter, motorOil);
+    public VehicleBuilder byDefault() {
+        VehicleBuilder vehicleBuilder = new VehicleBuilder("AA1234BB");
+        return vehicleBuilder
+                .setClient(new Client("fakeClient", 3))
+                .setBrand("Opel")
+                .setKMS("03-03-2017 94744")
+                .setBodyOnFrame("VF1KC0JEF31065732")
+                .setLastRevisionDate(LocalDate.now().minusMonths(2))
+                .setItvDate(LocalDate.now().minusMonths(3))
+                .setNextItvDate(LocalDate.now().plusYears(1))
+                .setAirFilterReference("1813029400")
+                .setOilFilterReference("1812344000")
+                .setFuelFilter("181315400")
+                .setMotorOil("5.5  5W30");
     }
+
+    public Vehicle createVehicle() {
+        return this.vehicle;
+    }
+
+
 }
