7fa57330a41a90de54ef9018714dd58f44d8a1e4
ilya <ilya.kab@gmail.com>
2019-04-22 20:14:46 +0200

    issue #56 añado el Dialog API para generar paginas como dialog en los que se mostrara la informacion

--- src/main/java/client_beans/clients/ClientsBean.java
+++ src/main/java/client_beans/clients/ClientsBean.java
@@ -1,11 +1,16 @@
 package client_beans.clients;
 
 import api.dtos.ClientDto;
+import org.primefaces.PrimeFaces;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ManagedBean
 @ViewScoped
@@ -17,6 +22,17 @@ public class ClientsBean implements Serializable {
     @PostConstruct
     public void init() {
         clients = new ClientGateway().readAll();
+    }
+
+    public void openClientDialog() {
+        List<String> clientParams = new ArrayList<>();
+        clientParams.add(Integer.toString(selectedClient.getId()));
+        Map<String, List<String>> params = new HashMap<>();
+        params.put("parameters", clientParams);
+        Map<String,Object> options = new HashMap<>();
+        options.put("resizable", false);
+        options.put("contentHeight", 320);
+        PrimeFaces.current().dialog().openDynamic("clientsInfo", options, params);
     }
 
     public List<ClientDto> getClients() {
