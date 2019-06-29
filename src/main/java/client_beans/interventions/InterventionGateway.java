0edda37c40204f6857309b037270f580c8d2a985
ilya <ilya.kab@gmail.com>
2019-06-29 12:51:06 +0200

    issue #70 Caso de uso listar Intervenciones. creo el gateway desde el cliente, y la pantalla de intervenciones. Adapto el ApiController

---
+++ src/main/java/client_beans/interventions/InterventionGateway.java
@@ -0,0 +1,76 @@
+package client_beans.interventions;
+
+import api.dtos.InterventionDto;
+import client_beans.util.PropertyLoader;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJsonProvider;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Properties;
+
+import static org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS;
+
+public class InterventionGateway {
+
+    Client client;
+    Properties properties;
+    private static final String API_PATH = "app.api.base.path";
+    public static final String APP_BASE_URL = "app.url";
+    public static final String INTERVENTIONS = "api.interventions.path";
+
+    private static final Logger LOGGER = LogManager.getLogger(InterventionGateway.class);
+
+    public InterventionGateway() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        JacksonJsonProvider jsonProvider = new JacksonJaxbJsonProvider(objectMapper, DEFAULT_ANNOTATIONS);
+        client = ClientBuilder.newClient().register(jsonProvider);
+        properties = new PropertyLoader().loadPropertiesFile("config.properties");
+    }
+
+    public String create(InterventionDto interventionDto) {
+        Response response = client.target(properties.getProperty(APP_BASE_URL) + properties.getProperty(API_PATH) + properties.getProperty(INTERVENTIONS))
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(interventionDto, MediaType.APPLICATION_JSON_TYPE));
+        LOGGER.info("API Crear intervention {} : Status: {}", interventionDto.getId(), response.getStatus());
+        return response.readEntity(String.class);
+    }
+
+    public Integer update(InterventionDto interventionDto) {
+        Response response = client.target(properties.getProperty(APP_BASE_URL) + properties.getProperty(API_PATH) + properties.getProperty(INTERVENTIONS) + "/" + interventionDto.getId())
+                .request(MediaType.APPLICATION_JSON)
+                .put(Entity.entity(interventionDto, MediaType.APPLICATION_JSON_TYPE));
+        LOGGER.info("API Actualizar intervention {} : Status: {}", interventionDto.getId(), response.getStatus());
+        return response.getStatus();
+    }
+
+    public List<InterventionDto> readAll() {
+        return client.target(properties.getProperty(APP_BASE_URL) + properties.getProperty(API_PATH) + properties.getProperty(INTERVENTIONS))
+                .request(MediaType.APPLICATION_JSON).get(new GenericType<List<InterventionDto>>() {
+                });
+    }
+
+    public InterventionDto read(String clientId) {
+        return client.target(properties.getProperty(APP_BASE_URL) + properties.getProperty(API_PATH) + properties.getProperty(INTERVENTIONS) + "/" + clientId)
+                .request(MediaType.APPLICATION_JSON)
+                .get(InterventionDto.class);
+    }
+
+    public void delete(int id) {
+        client.target(properties.getProperty(APP_BASE_URL) + properties.getProperty(API_PATH) + properties.getProperty(INTERVENTIONS) + "/" + id)
+                .request(MediaType.APPLICATION_JSON)
+                .delete();
+    }
+}
---
+++ src/main/java/client_beans/interventions/LazyInterventionsView.java
@@ -0,0 +1,160 @@
+package client_beans.interventions;
+
+import api.dtos.InterventionDto;
+import api.dtos.VehicleDto;
+import client_beans.clients.ClientGateway;
+import client_beans.vehicles.VehicleGateway;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ManagedBean(name = "lazyInterventionsView")
+@ViewScoped
+public class LazyInterventionsView implements Serializable {
+
+    private static final String NOT_NAME = "NOT NAME";
+    private LazyDataModel<InterventionDto> lazyModel;
+    private InterventionDto selectedInterventionDto;
+    private InterventionGateway interventionGateway = new InterventionGateway();
+    private VehicleGateway vehicleGateway = new VehicleGateway();
+    private ClientGateway clientGateway = new ClientGateway();
+
+    @PostConstruct
+    public void init() {
+        List<InterventionDto> intervetions = interventionGateway.readAll();
+//        intervetions.forEach(interventionDto -> clientNames.putIfAbsent(vehicle.getClientId(), clientGateway.read(vehicle.getClientId()).getFullName()));
+        lazyModel = new LazyDataModel<InterventionDto>() {
+
+            @Override
+            public int getRowCount() {
+                return interventionGateway.readAll().size();
+            }
+
+            @Override
+            public List<InterventionDto> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+
+                List<InterventionDto> filtered = interventionGateway.readAll().stream()
+                        .skip(first)
+                        //.filter(vehicleDto -> doFilter(vehicleDto, filters))
+                        //.filter(client -> client.getBrand().contains((String) filters.getOrDefault("brand", "")))
+                        .collect(Collectors.toList());
+
+                if (sortField == null) {
+                    return filtered;
+                }
+
+                return sortRows(sortField, sortOrder, filtered);
+            }
+
+            private List<InterventionDto> sortRows(String sortField, SortOrder sortOrder, List<InterventionDto> filtered) {
+                Comparator<InterventionDto> stateComparator = Comparator.comparing(InterventionDto::getState);
+                Comparator<InterventionDto> titleComparator = Comparator.comparing(InterventionDto::getTitle);
+
+                switch (sortField) {
+                    case "state":
+                        return filtered.stream().sorted(sortOrder == SortOrder.ASCENDING ? titleComparator : titleComparator.reversed()).collect(Collectors.toList());
+                    case "title":
+                        return filtered.stream().sorted(sortOrder == SortOrder.ASCENDING ? titleComparator : titleComparator.reversed()).collect(Collectors.toList());
+                    default:
+                        return null;
+                }
+            }
+
+//            private boolean doFilter(VehicleDto vehicleDto, Map<String, Object> filters) {
+//                if (filters == null || filters.isEmpty()) {
+//                    return true;
+//                }
+//
+//                return containsSearchString(vehicleDto.getRegistrationPlate(), filters.get("registrationPlate"))
+//                        && containsSearchString(vehicleDto.getBodyOnFrame(), filters.get("bodyOnFrame"))
+//                        && doFilterClientName(vehicleDto.getClientId(), filters.get("clientName"))
+//                        && globalContainsSearch(vehicleDto, filters.get("globalFilter"));
+//            }
+//
+//            private boolean doFilterClientName(String clientId, Object clientName) {
+//                if (!(clientName instanceof String)) {
+//                    return true;
+//                }
+//
+//                return clientNames.getOrDefault(clientId, NOT_NAME).toLowerCase().contains(((String) clientName).toLowerCase());
+//            }
+
+//            private boolean globalContainsSearch(VehicleDto vehicle, Object key) {
+//                if (!(key instanceof String)) {
+//                    return true;
+//                }
+//                String searchExpresion = ((String) key).toLowerCase();
+//                return vehicle.getRegistrationPlate().toLowerCase().contains(searchExpresion)
+//                        || vehicle.getBrand().toLowerCase().contains(searchExpresion)
+//                        || vehicle.getBodyOnFrame().toLowerCase().contains(searchExpresion)
+//                        || clientNames.getOrDefault(vehicle.getClientId(), NOT_NAME).toLowerCase().contains(searchExpresion);
+//            }
+
+            private boolean containsSearchString(String name, Object searchExpresion) {
+                if (!(searchExpresion instanceof String)) {
+                    return true;
+                }
+                return name.contains((String) searchExpresion);
+            }
+
+            @Override
+            public Integer getRowKey(InterventionDto interventionDto) {
+                return interventionDto.getId();
+            }
+
+            @Override
+            public InterventionDto getRowData(String rowKey) {
+                return interventionGateway.read(rowKey);
+            }
+        };
+    }
+
+    public LazyDataModel<InterventionDto> getLazyModel() {
+        return lazyModel;
+    }
+
+    public InterventionDto getSelectedInterventionDto() {
+        return selectedInterventionDto;
+    }
+
+    public void setSelectedInterventionDto(InterventionDto selectedInterventionDto) {
+        this.selectedInterventionDto = selectedInterventionDto;
+    }
+
+//    public String getClientDto(String clientId) {
+//        return getClientNames().getOrDefault(clientId, NOT_NAME);
+//    }
+
+
+    public void onRowSelect(SelectEvent event) {
+        String clientId = ((VehicleDto) event.getObject()).getClientId();
+    //    clientName = getClientNames().get(clientId);
+    }
+
+//    public String getClientName() {
+//        return clientName;
+//    }
+
+//    public void setClientName(String clientName) {
+//        this.clientName = clientName;
+//    }
+
+//    public Map<String, String> getClientNames() {
+//        return clientNames;
+//    }
+//
+//    public void setClientNames(Map<String, String> clientNames) {
+//        this.clientNames = clientNames;
+//    }
+}
+
