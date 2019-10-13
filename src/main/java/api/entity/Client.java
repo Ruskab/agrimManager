2f32d1f92602d04d832c8c60d0dcabab84cb2908
Ruskab <ilya.kab@gmail.com>
2019-10-13 23:14:14 +0200

    issue #54 fix sonar

--- src/main/java/api/entity/Client.java
+++ src/main/java/api/entity/Client.java
@@ -1,10 +1,13 @@
 package api.entity;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "client")
-public class Client {
+public class Client implements Serializable {
+
+    private static final long serialVersionUID = 1905122041950251207L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/api/entity/RepairingPack.java
+++ src/main/java/api/entity/RepairingPack.java
@@ -9,7 +9,6 @@ public class RepairingPack {
 
     private static final long serialVersionUID = 1905122041950251207L;
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
