92a80d8f242a6d0629938690fd544c31bbb032f1
ruskab <aq0866sw1343de>
2020-01-26 20:20:53 +0100

    issue #54 algunos sonars

--- src/main/java/api/business_controllers/AuthenticationBusinessController.java
+++ src/main/java/api/business_controllers/AuthenticationBusinessController.java
@@ -9,7 +9,7 @@ public class AuthenticationBusinessController {
 
     private MechanicBusinessController mechanicBusinessController = new MechanicBusinessController();
 
-    public void authenticateCredentials(String username, String password) {
+    public void authenticateCredentials(String username, String password) throws NotFoundException {
         List<MechanicDto> mechanicDtos = mechanicBusinessController.findBy(username);
         mechanicDtos.stream().filter(mechanicDto -> password.equals(mechanicDto.getPassword())).findFirst()
                 .orElseThrow(() -> new NotFoundException("Invalid Credentials"));
